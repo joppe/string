9e4fa9875859eebdf8a24f1258c6e4bae3cfffab
Joppe Aarts <joppe@apestaartje.info>
2017-01-12 22:04:24 +0100

    return types added

--- test/unit/string/repeat.spec.ts
+++ test/unit/string/repeat.spec.ts
@@ -1,23 +1,23 @@
 import * as string from '../../../src/string/repeat';
 
-describe('repeat', () => {
-    it('repeat the string by a given number', () => {
+describe('repeat', ():void => {
+    it('repeat the string by a given number', ():void => {
         expect(string.repeat('foo', 2)).toBe('foofoo');
     });
 
-    it('repeating an empty string will result in an empty string', () => {
+    it('repeating an empty string will result in an empty string', ():void => {
         expect(string.repeat('', 2)).toBe('');
     });
 
-    it('repeating a string 0 times will result in an empty string', () => {
+    it('repeating a string 0 times will result in an empty string', ():void => {
         expect(string.repeat('foo', 0)).toBe('');
     });
 
-    it('if a float is passed, cast it to integer', () => {
+    it('if a float is passed, cast it to integer', ():void => {
         expect(string.repeat('foobar', 3.9)).toBe('foobarfoobarfoobar');
     });
 
-    it('throw an error if the count smaller then zero', () => {
+    it('throw an error if the count smaller then zero', ():void => {
         expect(() => {
             string.repeat('foo', -1);
         }).toThrow();
