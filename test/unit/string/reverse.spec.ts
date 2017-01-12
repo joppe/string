c2aea7179cf98ef2b99c15e9fc6d2c0c571442d5
Joppe Aarts <joppe@apestaartje.info>
2017-01-12 22:04:43 +0100

    reverse function addd

---
+++ test/unit/string/reverse.spec.ts
@@ -0,0 +1,8 @@
+import {reverse} from '../../../src/string/reverse';
+
+describe('reverse', ():void => {
+    it('reverse a string', ():void => {
+        expect(reverse('hello world')).toBe('dlrow olleh');
+        expect(reverse(reverse('hello world'))).toBe('hello world');
+    });
+});
