b383432120444f9aaaa91970ba904a8d5f71e751
Joppe Aarts <joppe@zicht.nl>
2017-01-19 15:34:55 +0100

    removed compiled files thar are obsolete

--- dist/numberFormat.d.ts
+++
@@ -1,4 +0,0 @@
-export interface NumberFormatFunctionInterface {
-    (input: number, decimalCount?: number, decimalPoint?: string, thousandsSeperator?: string): string;
-}
-export declare const numberFormat: NumberFormatFunctionInterface;
