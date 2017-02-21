9e03306adf08638e9522270596833a230c7f54b9
Joppe Aarts <joppe@zicht.nl>
2017-02-21 16:32:34 +0100

    first working example

--- karma.conf.js
+++ karma.conf.js
@@ -1,86 +1,120 @@
-// Karma configuration
-
-
-var webpackConfig = require('./webpack.config.js');
+/**
+ * @author: @AngularClass
+ */
 
 module.exports = function (config) {
-    config.set({
+    var webpackConfig = require('./webpack.config.js');
 
-        // base path that will be used to resolve all patterns (eg. files, exclude)
-        basePath: './',
+    var configuration = {
 
+        // base path that will be used to resolve all patterns (e.g. files, exclude)
+        basePath: '',
 
-        // frameworks to use
-        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-        frameworks: [
-            'jasmine'
-        ],
-
+        /*
+         * Frameworks to use
+         *
+         * available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+         */
+        frameworks: ['jasmine'],
 
         // list of files to exclude
         exclude: [],
 
+        client: {
+            captureConsole: false
+        },
 
+        /*
+         * list of files / patterns to load in the browser
+         *
+         * we are building the test environment in ./spec-bundle.js
+         */
         files: [
             {
-                pattern: 'test/unit/**/*.spec.ts',
+                pattern: './spec-bundle.js',
                 watched: false
             }
         ],
 
+        /*
+         * preprocess matching files before serving them to the browser
+         * available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+         */
         preprocessors: {
-            'test/unit/**/*.spec.ts': ['webpack']
+            './spec-bundle.js': [
+                'coverage',
+                'webpack',
+                'sourcemap'
+            ]
         },
 
-        webpack: {
-            module: webpackConfig.module,
-            resolve: webpackConfig.resolve
+        // Webpack Config at ./webpack.test.js
+        webpack: webpackConfig,
+
+        coverageReporter: {
+            type: 'in-memory'
         },
 
+        remapCoverageReporter: {
+            'text-summary': null,
+            json: './coverage/coverage.json',
+            html: './coverage/html'
+        },
+
+        // Webpack please don't spam the console when running in karma!
         webpackMiddleware: {
             // webpack-dev-middleware configuration
-            // i. e.
-            stats: 'errors-only'
+            // i.e.
+            noInfo: true,
+
+            // and use stats to turn off verbose output
+            stats: {
+                // options i.e.
+                chunks: false
+            }
         },
 
-
-        plugins: [
-            'karma-jasmine',
-            'karma-phantomjs-launcher',
-            'karma-webpack'
+        /*
+         * test results reporter to use
+         *
+         * possible values: 'dots', 'progress'
+         * available reporters: https://npmjs.org/browse/keyword/karma-reporter
+         */
+        reporters: [
+            'progress',
+            'coverage',
+            'remap-coverage'
         ],
-
-
-        // test results reporter to use
-        // possible values: 'dots', 'progress'
-        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress', 'dots'],
-
 
         // web server port
         port: 9876,
 
-
         // enable / disable colors in the output (reporters and logs)
         colors: true,
 
-
-        // level of logging
-        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-        logLevel: config.LOG_INFO,
-
+        /*
+         * level of logging
+         * possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+         */
+        logLevel: config.LOG_WARN,
 
         // enable / disable watching file and executing tests whenever any file changes
-        autoWatch: true,
+        autoWatch: false,
 
+        /*
+         * start these browsers
+         * available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+         */
+        browsers: [
+            'PhantomJS'
+        ],
 
-        // start these browsers
-        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['PhantomJS'],
+        /*
+         * Continuous Integration mode
+         * if true, Karma captures browsers, runs the tests and exits
+         */
+        singleRun: true
+    };
 
-
-        // Continuous Integration mode
-        // if true, Karma captures browsers, runs the tests and exits
-        singleRun: false
-    })
+    config.set(configuration);
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,111 @@
-module.exports = {
-    output: {
-        filename: 'dist/bundle.js'
-    },
+/**
+ * @author: @AngularClass
+ */
 
+const path = require('path');
+
+/**
+ * Webpack configuration
+ *
+ * See: http://webpack.github.io/docs/configuration.html#cli
+ */
+const config = {
+    /**
+     * Source map for Karma from the help of karma-sourcemap-loader &  karma-webpack
+     *
+     * Do not change, leave as is or it wont work.
+     * See: https://github.com/webpack/karma-webpack#source-maps
+     */
+    devtool: 'inline-source-map',
+
+    /**
+     * Options affecting the resolving of modules.
+     *
+     * See: http://webpack.github.io/docs/configuration.html#resolve
+     */
     resolve: {
-        // Add `.ts` and `.tsx` as a resolvable extension.
-        extensions: ['.ts', '.tsx', '.js'] // note if using webpack 1 you'd also need a '' in the array as well
+
+        /**
+         * An array of extensions that should be used to resolve modules.
+         *
+         * See: http://webpack.github.io/docs/configuration.html#resolve-extensions
+         */
+        extensions: ['.ts', '.tsx', '.js']
     },
 
+    /**
+     * Options affecting the normal modules.
+     *
+     * See: http://webpack.github.io/docs/configuration.html#module
+     *
+     * 'use:' revered back to 'loader:' as a temp. workaround for #1188
+     * See: https://github.com/AngularClass/angular2-webpack-starter/issues/1188#issuecomment-262872034
+     */
     module: {
-        loaders: [ // loaders will work with webpack 1 or 2; but will be renamed "rules" in future
-            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-            { test: /\.tsx?$/, loader: 'ts-loader' }
+        rules: [
+            /**
+             * Source map loader support for *.js files
+             * Extracts SourceMaps for source files that as added as sourceMappingURL comment.
+             *
+             * See: https://github.com/webpack/source-map-loader
+             */
+            {
+                enforce: 'pre',
+                test: /\.js$/,
+                loader: 'source-map-loader'
+            },
+
+            /**
+             * Typescript loader support for .ts
+             *
+             * See: https://github.com/s-panferov/awesome-typescript-loader
+             */
+            {
+                test: /\.ts$/,
+                use: [
+                    {
+                        loader: 'awesome-typescript-loader',
+                        query: {
+                            // use inline sourcemaps for "karma-remap-coverage" reporter
+                            sourceMap: false,
+                            inlineSourceMap: true,
+                            compilerOptions: {
+                                // Remove TypeScript helpers to be injected
+                                // below by DefinePlugin
+                                removeComments: true
+                            }
+                        }
+                    }
+                ]
+            },
+
+            /**
+             * Instruments JS files with Istanbul for subsequent code coverage reporting.
+             * Instrument only testing sources.
+             *
+             * See: https://github.com/deepsweet/istanbul-instrumenter-loader
+             */
+            {
+                enforce: 'post',
+                test: /\.(js|ts)$/,
+                loader: 'istanbul-instrumenter-loader',
+                include: path.resolve('src'),
+                exclude: [
+                    /\.(spec)\.ts$/,
+                    /node_modules/
+                ]
+            }
+
         ]
-    }
+    },
+
+    /**
+     * Add additional plugins to the compiler.
+     *
+     * See: http://webpack.github.io/docs/configuration.html#plugins
+     */
+    plugins: [
+    ]
 };
+
+module.exports = config;
