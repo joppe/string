bcee1476064144a50a6834c0d4bdeca829718721
Joppe Aarts <joppe@apestaartje.info>
2018-05-18 12:20:31 +0200

    Removed css/sass related stuff from webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const config = {
     devtool: 'source-map',
@@ -24,44 +23,6 @@ const config = {
                 enforce: 'pre',
                 test: /\.js$/,
                 loader: 'source-map-loader'
-            },
-
-            {
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: [
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                sourceMap: true
-                            }
-                        },
-
-                        {
-                            loader: 'postcss-loader',
-                            options: {
-                                plugins: () => {
-                                    return [
-                                        require('autoprefixer')({
-                                            browsers: [
-                                                'last 2 versions',
-                                                'IE >= 9'
-                                            ]
-                                        })
-                                    ];
-                                },
-                                sourceMap: true
-                            }
-                        },
-                        {
-                            loader: 'sass-loader',
-                            options: {
-                                sourceMap: true
-                            }
-                        }
-                    ]
-                })
             }
         ]
     },
@@ -70,14 +31,9 @@ const config = {
         path: path.resolve(__dirname, 'dist'),
         publicPath: '/dist'
     },
-    plugins: [
-        new ExtractTextPlugin({
-            filename: 'main.css'
-        }),
-    ],
     resolve: {
         alias: {
-            app: path.resolve(__dirname, 'src')
+            number: path.resolve(__dirname, 'src')
         },
         extensions: [
             '.js', '.ts', '.tsx'
