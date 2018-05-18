ef985dab11acc6697672363122a542beb4b32907
Joppe Aarts <joppe@apestaartje.info>
2018-05-18 12:10:47 +0200

    Remove compiled JavaScript and declaration files

--- dist/repeat.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.repeat = (input, count) => {
-    if (0 > count) {
-        throw new Error(`Count must be a positive number, "${count}" given.`);
-    }
-    return new Array(Math.floor(count) + 1).join(input);
-};
-//# sourceMappingURL=repeat.js.map
--- dist/reverse.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.reverse = (input) => {
-    return input.split('').reverse().join('');
-};
-//# sourceMappingURL=reverse.js.map
