0758116b3bd528b94d61a459f417df3055ac26a5
Joppe Aarts <joppe@apestaartje.info>
2018-05-21 16:30:27 +0200

    Unit test tweaks

--- test/unit/repeat.spec.ts
+++ test/unit/repeat.spec.ts
@@ -1,32 +1,32 @@
-import * as string from '../../src/repeat';
+import { repeat } from '../../src/repeat';
 
 describe('repeat', (): void => {
     it('repeat the string by a given number', (): void => {
-        expect(string.repeat('foo', 2)).toBe('foofoo');
+        expect(repeat('foo', 2)).toBe('foofoo');
     });
 
     it('repeating an empty string will result in an empty string', (): void => {
-        expect(string.repeat('', 2)).toBe('');
+        expect(repeat('', 2)).toBe('');
     });
 
     it('repeating a string less then 1 time will result in an empty string', (): void => {
         expect(() => {
-            string.repeat('foo', -3);
+            repeat('foo', -3);
         }).toThrow();
         expect(() => {
-            string.repeat('foo', 0);
+            repeat('foo', 0);
         }).toThrow();
     });
 
     it('the number of times to repeat must be an integer', (): void => {
         expect(() => {
-            string.repeat('foo', 3.9);
+            repeat('foo', 3.9);
         }).toThrow();
     });
 
     it('throw an error if the count smaller then zero', (): void => {
         expect(() => {
-            string.repeat('foo', -1);
+            repeat('foo', -1);
         }).toThrow();
     });
 });
