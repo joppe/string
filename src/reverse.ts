e326580ebfe549d120fa85729500048be12b539f
Joppe Aarts <joppe@apestaartje.info>
2018-06-05 21:09:45 +0200

    Added whitespace

--- src/reverse.ts
+++ src/reverse.ts
@@ -2,7 +2,7 @@
  * This function reverses the order of the characters in a string.
  */
 
-export type ReverseFunction= (input: string) => string;
+export type ReverseFunction = (input: string) => string;
 
 export const reverse: ReverseFunction = (input: string): string => {
     return input.split('').reverse().join('');
